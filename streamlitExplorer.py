3db02a0091438220b20f0f9dbdb63333e05fe4c0
franciscolopes <franciscolopesdacunha@gmail.com>
2022-12-12 17:52:36 +0000

    Adds scikit-learn models

--- streamlitExplorer.py
+++ streamlitExplorer.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pylab as plt
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import AdaBoostRegressor
 
 
 st.title("Simulation[tm]")
@@ -11,20 +13,43 @@ st.sidebar.markdown("## Controls")
 st.sidebar.markdown("Change **parameters** bellow to refresh both plots")
 
 #Adds slider on sidebar
-x = st.sidebar.slider('Slope', min_value=0.01, max_value=0.10, step=0.01)
-y = st.sidebar.slider('Noise', min_value=0.01, max_value=0.10, step=0.01)
+xAxis = st.sidebar.slider('Slope', min_value=0.01, max_value=0.10, step=0.01)
+yAxis = st.sidebar.slider('Noise', min_value=0.01, max_value=0.10, step=0.01)
+n_est = st.sidebar.slider("n_est", min_value=1, max_value=5_000, step=1)
 
-st.write(f"x={x} y={y}")
+
+st.write(f"x={xAxis} y={yAxis}")
+
+#simulated data
+values = np.cumprod(1 + np.random.normal(xAxis, yAxis, (100,10)), axis=0)
+n = 1000
+np.random.seed(42)
+x = np.linspace(0, 6, n)
+X = np.linspace(0, 6, n)[:, np.newaxis]
+y = np.sin(X).ravel() + np.sin(6 * X).ravel() + np.random.random(n) * 0.3
 
 #builds line chart using streamlit built-in function
-values = np.cumprod(1 + np.random.normal(x, y, (100,10)), axis=0)
 st.line_chart(values)
 
 #builds line chart using matplotlib
 fig, ax = plt.subplots()
-
 for i in range(values.shape[1]):
     ax.plot(values[:, i])
 
-plt.title(f"x={x} y={y}")
+plt.title(f"x={xAxis} y={yAxis}")
 st.pyplot(fig)
+
+#Adds scikit-learn models
+mod1 = DecisionTreeRegressor(max_depth=4)
+y1 = mod1.fit(X,y).predict(X)
+y2 = AdaBoostRegressor(mod1, n_estimators=n_est).fit(X, y).predict(X)
+
+#Add model predictions plot 
+fig2, ax2 = plt.subplots()
+ax2.scatter(x, y, alpha=0.1)
+ax2.plot(x, y1, label="just a tree")
+ax2.plot(x, y2, label=f"adaboost-{n_est}")
+ax2.legend()
+st.pyplot(fig2)
+
+
