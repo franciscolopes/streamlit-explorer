7dc868fed2b73a3cde569c1eb0ed039edaa77cf4
franciscolopes <franciscolopesdacunha@gmail.com>
2022-12-12 16:26:50 +0000

    Adds line chart

--- streamlitExplorer.py
+++ streamlitExplorer.py
@@ -10,3 +10,5 @@ y = st.slider('Noise', min_value=0.01, max_value=0.10, step=0.01)
 
 st.write(f"x={x} y={y}")
 
+values = np.cumprod(1 + np.random.normal(x, y, (100,10)), axis=0)
+st.line_chart(values)
